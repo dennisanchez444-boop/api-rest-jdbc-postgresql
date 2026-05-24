38e115e34d3c14792129fd2ced29b319a5e89648
dennisanchez444-boop <dennisanchez444@gmail.com>
2026-05-24 18:18:12 -0500

    DDSQ:Se creo la Clase JDBC

---
+++ src/main/java/com/krakedev/jdbc/videojuegos/VideojuegoJdbc.java
@@ -0,0 +1,121 @@
+package com.krakedev.jdbc.videojuegos;
+
+import com.krakdev.videojuegos.entidades.Videojuego;
+import com.krakedev.jdbc.Conexion;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class VideojuegoJdbc {
+
+	// INSERTAR
+	public Videojuego insertar(Videojuego videojuego) {
+		String sql = """
+				INSERT INTO videojuegos
+				(codigo,nombre,plataforma,precio,disponible,genero)
+				VALUES (?,?,?,?,?,?)
+				""";
+
+		try (Connection con = Conexion.conectar(); PreparedStatement ps = con.prepareStatement(sql)) {
+			ps.setString(1, videojuego.getCodigo());
+			ps.setString(2, videojuego.getNombre());
+			ps.setString(3, videojuego.getPlataforma());
+			ps.setDouble(4, videojuego.getPrecio());
+			ps.setBoolean(5, videojuego.isDisponible());
+			ps.setString(6, videojuego.getGenero());
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return videojuego;
+	}
+
+	// LISTAR
+	public List<Videojuego> listar() {
+		List<Videojuego> lista = new ArrayList<>();
+		String sql = "SELECT * FROM videojuegos";
+
+		try (Connection con = Conexion.conectar();
+				PreparedStatement ps = con.prepareStatement(sql);
+				ResultSet rs = ps.executeQuery()) {
+			while (rs.next()) {
+				Videojuego v = new Videojuego();
+				v.setCodigo(rs.getString("codigo"));
+				v.setNombre(rs.getString("nombre"));
+				v.setPlataforma(rs.getString("plataforma"));
+				v.setPrecio(rs.getDouble("precio"));
+				v.setDisponible(rs.getBoolean("disponible"));
+				v.setGenero(rs.getString("genero"));
+
+				lista.add(v);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return lista;
+	}
+
+	// BUSCAR
+	public Videojuego buscar(String codigo) {
+		Videojuego v = null;
+		String sql = "SELECT * FROM videojuegos WHERE codigo = ?";
+		try (Connection con = Conexion.conectar(); PreparedStatement ps = con.prepareStatement(sql)) {
+			ps.setString(1, codigo);
+
+			ResultSet rs = ps.executeQuery();
+
+			if (rs.next()) {
+				v = new Videojuego();
+				v.setCodigo(rs.getString("codigo"));
+				v.setNombre(rs.getString("nombre"));
+				v.setPlataforma(rs.getString("plataforma"));
+				v.setPrecio(rs.getDouble("precio"));
+				v.setDisponible(rs.getBoolean("disponible"));
+				v.setGenero(rs.getString("genero"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return v;
+	}
+
+	// ACTUALIZAR
+	public Videojuego actualizar(String codigo, Videojuego videojuego) {
+
+		String sql = """
+				UPDATE videojuegos
+				SET nombre=?,
+				    plataforma=?,
+				    precio=?,
+				    disponible=?,
+				    genero=?
+				WHERE codigo=?
+				""";
+		try (Connection con = Conexion.conectar(); PreparedStatement ps = con.prepareStatement(sql)) {
+			ps.setString(1, videojuego.getNombre());
+			ps.setString(2, videojuego.getPlataforma());
+			ps.setDouble(3, videojuego.getPrecio());
+			ps.setBoolean(4, videojuego.isDisponible());
+			ps.setString(5, videojuego.getGenero());
+			ps.setString(6, codigo);
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return videojuego;
+	}
+
+	// ELIMINAR
+	public boolean eliminar(String codigo) {
+		String sql = "DELETE FROM videojuegos WHERE codigo=?";
+		try (Connection con = Conexion.conectar(); PreparedStatement ps = con.prepareStatement(sql)) {
+			ps.setString(1, codigo);
+			int filas = ps.executeUpdate();
+			return filas > 0;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+}
