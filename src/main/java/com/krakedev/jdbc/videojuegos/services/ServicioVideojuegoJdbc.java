d58a38716ed1908028a092bf326b8a250a48d7b1
dennisanchez444-boop <dennisanchez444@gmail.com>
2026-05-24 18:20:39 -0500

    DDSQ:Se creo la clase servicio de videojuego

---
+++ src/main/java/com/krakedev/jdbc/videojuegos/services/ServicioVideojuegoJdbc.java
@@ -0,0 +1,35 @@
+package com.krakedev.jdbc.videojuegos.services;
+import com.krakdev.videojuegos.entidades.Videojuego;
+import com.krakedev.jdbc.videojuegos.VideojuegoJdbc;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class ServicioVideojuegoJdbc {
+    private VideojuegoJdbc jdbc = new VideojuegoJdbc();
+
+    // CREAR
+    public Videojuego crear(Videojuego videojuego) {
+        return jdbc.insertar(videojuego);
+    }
+
+    // LISTAR
+    public List<Videojuego> listar() {
+        return jdbc.listar();
+    }
+
+    // BUSCAR
+    public Videojuego buscarPorCodigo(String codigo) {
+        return jdbc.buscar(codigo);
+    }
+
+    // ACTUALIZAR
+    public Videojuego actualizar(String codigo, Videojuego videojuego) {
+        return jdbc.actualizar(codigo, videojuego);
+    }
+
+    // ELIMINAR
+    public boolean eliminar(String codigo) {
+        return jdbc.eliminar(codigo);
+    }
+}
