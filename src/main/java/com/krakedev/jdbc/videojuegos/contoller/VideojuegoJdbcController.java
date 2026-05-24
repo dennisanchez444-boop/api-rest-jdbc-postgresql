075f7b5b538096c4e6f15425ada317fa17da9b7d
dennisanchez444-boop <dennisanchez444@gmail.com>
2026-05-24 18:23:04 -0500

    DDSQ:Se creo la clase controllerJdbc

---
+++ src/main/java/com/krakedev/jdbc/videojuegos/contoller/VideojuegoJdbcController.java
@@ -0,0 +1,42 @@
+package com.krakedev.jdbc.videojuegos.contoller;
+
+import com.krakdev.videojuegos.entidades.Videojuego;
+import com.krakedev.jdbc.videojuegos.services.ServicioVideojuegoJdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/jdbc/videojuegos")
+public class VideojuegoJdbcController {
+
+	@Autowired
+	private ServicioVideojuegoJdbc service;
+
+	@PostMapping
+	public Videojuego crear(@RequestBody Videojuego videojuego) {
+		return service.crear(videojuego);
+	}
+
+	@GetMapping
+	public List<Videojuego> listar() {
+		return service.listar();
+	}
+
+	@GetMapping("/{codigo}")
+	public Videojuego buscar(@PathVariable String codigo) {
+		return service.buscarPorCodigo(codigo);
+	}
+
+	@PutMapping("/{codigo}")
+	public Videojuego actualizar(@PathVariable String codigo, @RequestBody Videojuego videojuego) {
+		return service.actualizar(codigo, videojuego);
+	}
+
+	@DeleteMapping("/{codigo}")
+	public boolean eliminar(@PathVariable String codigo) {
+		return service.eliminar(codigo);
+	}
+}
